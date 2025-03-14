c3ff86c1e6716a123635864e718830afb2133d6c
Kailie Field <kailie.field@gmail.com>
2025-03-14 13:50:39 -0400

    removing views

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = 3000;
 // ---- Serving static files (HTML, CSS, JS, Images)----
 app.use(express.static(path.join(__dirname, "public")))
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 // ---- START SERVER ----
 app.listen(PORT, () => {
     console.log(`[SERVER RUNNING ON : ${PORT}]`);
