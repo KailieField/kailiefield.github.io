48c8f81dff6c27e257d3ccad37316ac722e65c04
Kailie Field <kailie.field@gmail.com>
2025-03-14 13:46:05 -0400

    initializing portofilio website directory structure and baseline documents

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// ---- Serving static files (HTML, CSS, JS, Images)----
+app.use(express.static(path.join(__dirname, "public")))
+
+// ---- START SERVER ----
+app.listen(PORT, () => {
+    console.log(`[SERVER RUNNING ON : ${PORT}]`);
+});
